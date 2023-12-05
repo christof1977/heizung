587364b8e2babdad7db513cd4301b375c6360d6c
Christof <github@burningwheels.com>
2023-12-05 08:21:08 +0100

    Changed first nibble of MAC

--- setmac.sh
+++ setmac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-raspberry="B8:27:EB:" # Die ersten drei Bytes der MAC-Adresse sind statisch
+raspberry="C8:27:EB:" # Die ersten drei Bytes der MAC-Adresse sind statisch
 # Die letzten drei Bytes werden aus der CPU-ID generiert:
 cpuid=$(cat /proc/cpuinfo | grep Serial | cut -c 21-26 | sed 's/\(..\)/\1:/g')
 
